9af3d8cb495f03ec6cbd51e4b6e557ba213beb39
EnricoDErasmo <enrico.derasmo@gmail.com>
2023-02-23 17:16:56 +0100

    created js program

--- js/script.js
+++ js/script.js
@@ -12,3 +12,61 @@ MILESTONE 2:
 Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 */
+
+/*
+- chiedere numero di km
+- chiedere età passeggero
+- calcolare il prezzo totale del viaggio
+?SE il passeggero ha meno di 18 anni:
+ °sconto 20%
+ALTRIMENTI se il passeggero ha 65 anni o più:
+ °sconto 40%
+*/
+
+
+// Imposto variabili per dati utente
+let userKm = parseInt(document.getElementById("userKm").value);
+let userAge = parseInt(document.getElementById("userAge").value);
+
+// Imposto una variabile per il button
+let searchBtnEl = document.getElementById("searchBtn");
+
+// Imposto variabili per i prezzi e sconti
+let price = (0.21 * userKm).toFixed(2);
+let discount;
+let totalPrice;
+
+// Creo un evento con il click
+searchBtnEl.addEventListener('click', function() {
+
+    if (userAge < 18) {
+    
+        discount = ((price / 100) * 20).toFixed(2);
+        
+        totalPrice = (price - discount).toFixed(2);
+
+        console.log(totalPrice);
+    
+    } else if (userAge >= 65) {
+    
+        discount = ((price / 100) * 40).toFixed(2);
+    
+        totalPrice = (price - discount).toFixed(2);
+
+        console.log(totalPrice);
+
+    } else {
+
+        totalPrice = price
+
+        console.log(totalPrice);
+
+    }
+});
+
+
+
+
+    
+
+
